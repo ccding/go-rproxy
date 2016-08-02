6414233cd7657de1a69b4805c49dfcf014c44a01
Cong Ding <dinggnu@gmail.com>
2016-08-02 16:48:57 -0400

    avoid loading client certificate multiple times

--- rproxy/rproxy.go
+++ rproxy/rproxy.go
@@ -37,6 +37,8 @@ type RProxy struct {
 	serverKey    string
 	clientCert   string
 	clientKey    string
+	clientConfig *tls.Config
+	serverConfig *tls.Config
 }
 
 func NewRProxy(listenProto, listenAddr, backendProto, backendAddr, rootCert, serverCert, serverKey, clientCert, clientKey string) *RProxy {
@@ -53,18 +55,52 @@ func NewRProxy(listenProto, listenAddr, backendProto, backendAddr, rootCert, ser
 	}
 }
 
+func (rp *RProxy) SetClientConfig(config *tls.Config) {
+	rp.clientConfig = config
+}
+
+func (rp *RProxy) SetServerConfig(config *tls.Config) {
+	rp.serverConfig = config
+}
+
 func (rp *RProxy) Start() {
+	// Check backend protocol and load certificates if TLS
+	switch rp.backendProto {
+	case "tcp":
+	case "tls":
+		// Load client certificates for TLS
+		if rp.clientConfig == nil {
+			serverName := "testapp-server"
+			config, err := certs.LoadClientCerts(rp.rootCert, rp.clientCert, rp.clientKey, serverName)
+			if err != nil {
+				panic(err)
+			}
+			rp.clientConfig = config
+		}
+	default:
+		panic("backend protocol not supported")
+	}
+	// Check listen protocol, load certiticates if TLS, and start listening
 	switch rp.listenProto {
 	case "tcp":
 		rp.startTCP()
 	case "tls":
+		// Load server certificates for TLS
+		if rp.serverConfig == nil {
+			config, err := certs.LoadServerCerts(rp.rootCert, rp.serverCert, rp.serverKey)
+			if err != nil {
+				panic(err)
+			}
+			rp.serverConfig = config
+		}
+		// Start listening through TLS protocol
 		rp.startTLS()
 	default:
 		panic("listen protocol not supported")
 	}
 }
 
-func (rp *RProxy) serve(conn net.Conn) error {
+func (rp *RProxy) serve(conn net.Conn) {
 	switch rp.backendProto {
 	case "tcp":
 		rp.serveTCP(conn)
@@ -73,7 +109,6 @@ func (rp *RProxy) serve(conn net.Conn) error {
 	default:
 		panic("backend protocol not supported")
 	}
-	return nil
 }
 
 func (rp *RProxy) startTCP() {
@@ -100,12 +135,8 @@ func (rp *RProxy) startTCP() {
 }
 
 func (rp *RProxy) startTLS() {
-	config, err := certs.LoadServerCerts(rp.rootCert, rp.serverCert, rp.serverKey)
-	if err != nil {
-		panic(err)
-	}
 	// Listen to TLS connections
-	ln, err := tls.Listen("tcp", rp.listenAddr, config)
+	ln, err := tls.Listen("tcp", rp.listenAddr, rp.serverConfig)
 	if err != nil {
 		panic(err)
 	}
@@ -142,13 +173,8 @@ func (rp *RProxy) serveTCP(listenConn net.Conn) error {
 }
 
 func (rp *RProxy) serveTLS(listenConn net.Conn) error {
-	serverName := "testapp-server"
-	config, err := certs.LoadClientCerts(rp.rootCert, rp.clientCert, rp.clientKey, serverName)
-	if err != nil {
-		return err
-	}
 	// Dial to the beckend server
-	backendConn, err := tls.Dial("tcp", rp.backendAddr, config)
+	backendConn, err := tls.Dial("tcp", rp.backendAddr, rp.clientConfig)
 	if err != nil {
 		listenConn.Close()
 		return err
